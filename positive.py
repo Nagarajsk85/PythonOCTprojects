716c621b0162508100ba06982cec9e8cffeb5692
Nagarajsk85 <69401762+Nagarajsk85@users.noreply.github.com>
2023-10-18 17:27:36 +0530

    Add files via upload

---
+++ positive.py
@@ -0,0 +1,13 @@
+# Input list of numbers
+list1 = [12, -7, 5, 64, -14]
+list2 = [12, 14, -95, 3]
+# Iterate through the list1 and print positive numbers
+print("Positive numbers in the list1:")
+for num in list1:
+    if num > 0:
+        print(num)
+# Iterate through the list2 and print positive numbers 
+print("\nPositive numbers in the list2:")
+for num in list2:
+    if num > 0:
+        print([num])
